f0a6fb219ba6eee1c54e626d8ad44affc769a153
younhong <younhong@kakao.com>
2021-01-16 01:48:55 +0900

    update

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts';
 import router from './routes.ts';
 
-const port = 5000;
+const port = Deno.env.get("PORT") || 5000;
 const app = new Application();
 
 app.use(router.routes());
@@ -13,4 +13,4 @@ router.get('/api/v1/products', ({response}: {response: any}) => {
 
 console.log(`Server running on port ${port}`);
 
-await app.listen({ port });
+await app.listen({ port: +port });
