4392bd41609fabddc5c674756e846da1a4078d0f
younhong <younhong@kakao.com>
2021-01-16 15:20:00 +0900

    Revert "update"
    This reverts commit f0a6fb219ba6eee1c54e626d8ad44affc769a153.

--- config.ts
+++
@@ -1,9 +0,0 @@
-const dbCreds = {
-    user: "your postgresql user name",
-    database: "your db user name",
-    password: "your pw",
-    hostname: "localhost",
-    port: 5433
-}
-
-export {dbCreds}
